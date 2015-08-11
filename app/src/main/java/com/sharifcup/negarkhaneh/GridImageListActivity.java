0689fa875da1957967e00bd90c0ff7267979e4b9
Hamid <hp.hamid71@gmail.com>
2015-08-11 22:26:13 +0430

    Day 1

---
+++ app/src/main/java/com/sharifcup/negarkhaneh/GridImageListActivity.java
@@ -0,0 +1,11 @@
+package com.sharifcup.negarkhaneh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GridImageListActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
