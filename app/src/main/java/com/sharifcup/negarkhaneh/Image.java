30bd7d3a0b6dd21cc230800b11f52ad83613428b
Hamid <hp.hamid71@gmail.com>
2015-08-11 22:28:55 +0430

    Day 2

---
+++ app/src/main/java/com/sharifcup/negarkhaneh/Image.java
@@ -0,0 +1,25 @@
+package com.sharifcup.negarkhaneh;
+
+import java.io.Serializable;
+
+public class Image implements Serializable {
+    private int imageAddress;
+    private String imageTitle;
+
+
+    public String getImageTitle() {
+        return imageTitle;
+    }
+
+    public void setImageTitle(String imageTitle) {
+        this.imageTitle = imageTitle;
+    }
+
+    public int getImageAddress() {
+        return imageAddress;
+    }
+
+    public void setImageAddress(int imageAddress) {
+        this.imageAddress = imageAddress;
+    }
+}
